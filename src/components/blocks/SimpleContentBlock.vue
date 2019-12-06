1321fff31375d6a7fc991556b24f78229e6f9314
Stephen <fowleswhite@gmail.com>
2019-12-06 08:38:29 +0000

    updated the Simple content block to be a dynamic block

--- src/components/blocks/SimpleContentBlock.vue
+++ src/components/blocks/SimpleContentBlock.vue
@@ -1,10 +1,10 @@
 <template>
-<div class="container mt-4">
-    <div class="row">
+<div class="container mt-5 mb-5">
+    <div class="row" :class="{ c_direction : getisRevered}">
         <div class="col-md-6 c-simple-content__content__container">
-            <h2 class="c-simple-content__title u__text__color__dark__blue">HTML & CSS</h2>
-            <h4 class="c-simple-content__subtitle mb-5 u__text__dark-base-green">Foundations of Web Design</h4>
-            <div class="c-simple-content__content u__text__color__dark__blue">Some text</div>
+            <h2 class="c-simple-content__title u__text__color__dark__blue">{{ getTitle }}</h2>
+            <h4 class="c-simple-content__subtitle mb-3 u__text__dark-base-green text-uppercase"> {{ getSubtitle }}</h4>
+            <div class="c-simple-content__content u__text__color__dark__blue">{{getContent }}</div>
             <a class="c-simple-content__button" href="">Link</a>
         </div>
         <div class="col-md-6">
@@ -16,7 +16,64 @@
 </template>
 <script>
 export default {
-    name: 'SimpleContentBlock'
+    name: 'SimpleContentBlock',
+    props: {
+        block: {
+            type: Object,
+            default: undefined
+        }
+    },
+    computed: {
+        getTitle(){
+            if(this.block.title !== undefined || this.block.title !== null){
+                return this.block.title;
+            }else{
+                return undefined;
+            }
+        },
+        getSubtitle(){
+            if(this.block.subtitle !== undefined || this.block.subtitle !== null){
+                return this.block.subtitle;
+            }else{
+                return undefined;
+            }
+        },
+        getContent(){
+            if(this.block.content !== undefined || this.block.content !== null){
+                return this.block.content;
+            }else{
+                return undefined;
+            }
+        },
+        getLink(){
+            if(this.block.link !== undefined || this.block.link !== null){
+                return this.block.link;
+            }else{
+                return undefined;
+            }
+        },
+        getLinkText(){
+            if(this.block.linkText !== undefined || this.block.linkTExt !== null){
+                return this.block.linkText;
+            }else{
+                return undefined;
+            }
+        },
+        getImage(){
+            if(this.block.image !== undefined || this.block.image !== null){
+                return this.block.image;
+            }else{
+                return undefined;
+            }
+        },
+        getisRevered(){
+            if(this.block.reversed !== undefined || this.block.reversed !== null){
+                return this.block.reversed;
+            }else{
+                return undefined;
+            }
+        },
+    }
     
 }
 </script>
@@ -32,12 +89,15 @@ export default {
     margin-bottom: 0rem;
 }
 .c-simple-content__subtitle{
-    font-size:1.2rem;
+    font-size:1.1rem;
     line-height: 1.5rem;
 }
 .c-simple-content__img{
     width: 100%;
     height: auto;
     object-fit: cover;
+}
+.c_direction{
+    flex-direction: row-reverse;
 }
 </style>
--- src/components/pages/AboutMe.vue
+++ src/components/pages/AboutMe.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="">
         <header-banner :header="getHeaderBanner"></header-banner>
-        <simple-content-block></simple-content-block>
+        <simple-content-block :block="getsimpleContentBlock1"></simple-content-block>
+        <simple-content-block :block="getsimpleContentBlock2"></simple-content-block>
+        <simple-content-block :block="getsimpleContentBlock3"></simple-content-block>
         <content-block :block="getContent"></content-block>
     </div>
 </template>
@@ -26,6 +28,27 @@ export default {
         getContent(){
             if(this.AboutMeData.contentBlock !== null || this.AboutMeData.contentBlock !== undefined){
                     return this.AboutMeData.contentBlock;
+                } else {
+                    return undefined;
+            }
+        },
+        getsimpleContentBlock1(){
+            if(this.AboutMeData.simpleContentBlock1 !== null || this.AboutMeData.simpleContentBlock1 !== undefined){
+                    return this.AboutMeData.simpleContentBlock1;
+                } else {
+                    return undefined;
+            }
+        },
+        getsimpleContentBlock2(){
+            if(this.AboutMeData.simpleContentBlock2 !== null || this.AboutMeData.simpleContentBlock2 !== undefined){
+                    return this.AboutMeData.simpleContentBlock2;
+                } else {
+                    return undefined;
+            }
+        },
+        getsimpleContentBlock3(){
+            if(this.AboutMeData.simpleContentBlock3 !== null || this.AboutMeData.simpleContentBlock3 !== undefined){
+                    return this.AboutMeData.simpleContentBlock3;
                 } else {
                     return undefined;
             }
